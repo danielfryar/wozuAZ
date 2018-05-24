e618ea1d6b2c497dbf812386e4dcd0897e1a88d3
Daniel Fryar <daniel.fryar@gmail.com>
2018-05-24 15:31:03 -0700

    Made a few css tweaks, but moving on with js

--- day4/colors.js
+++ day4/colors.js
@@ -1,2 +1,17 @@
+let changeColor = function(color) {
+    console.log("inside function", color)
+    document.getElementById("main")
+        .style.backgroundColor = color;
+}
 
-document.getElementById("main").style.backgroundColor = "skyblue";
+let colors = ["magenta","orange","green","cyan","yellow"];
+
+for (let i = 0; i< 500000000; i++) {
+    if (i%100000000 == 0) {
+        colorIndex = i/100000000;
+        changeColor(colors[colorIndex]);
+        console.log(i, colorIndex,colors[colorIndex]);
+    }
+}
+
+changeColor(colors[2]);
