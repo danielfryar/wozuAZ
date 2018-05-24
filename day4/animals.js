000dee5fd229122944cbde4c5377942f0592320f
Daniel Fryar <daniel.fryar@gmail.com>
2018-05-24 11:11:59 -0700

    saving array lesson in animals

---
+++ day4/animals.js
@@ -0,0 +1,16 @@
+let zoo = [
+    "bear",
+    "cardinal",
+    "kitten",
+    "elephant",
+    "rabbit"
+];
+zoo.push("snake");
+zoo.push("frog");
+while (zoo.length > 0) {
+    console.log(zoo.pop());
+}
+
+//zoo[]
+//zoo.indexOf("animal")
+//zoo.splice("target",1)
